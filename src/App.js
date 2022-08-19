e879f1f283483f46efd789ff9588c86d32f892a6
Adityakanojia24 <adityakanojia63830@gmail.com>
2022-08-19 10:11:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/navbar';
 import Center from './components/center';
 import Timer from './components/timer';
 import Clock from './components/clock';
+import Password from './components/PasswordGenerator';
 import List from './components/list';
 import Home from './components/home';
 import Footer from './components/footer';
@@ -18,6 +19,7 @@ const App=()=>{
     <List/>
     <Timer/>
     <Clock/>
+    <Password/>
     <Home/>
     <Footer/>
    

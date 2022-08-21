0bfa0e26205aa7bfe9c867551007d0d9f7da341b
Adityakanojia24 <adityakanojia63830@gmail.com>
2022-08-21 17:02:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Navbar from './components/navbar';
-import Center from './components/center';
-import Timer from './components/timer';
-import Clock from './components/clock';
-import Password from './components/PasswordGenerator';
-import List from './components/list';
-import Home from './components/home';
-import Footer from './components/footer';
+// import Navbar from './components/navbar';
+// import Center from './components/center';
+// import Timer from './components/timer';
+// import Clock from './components/clock';
+// import Password from './components/PasswordGenerator';
+// import List from './components/list';
+// import Home from './components/home';
+// import Footer from './components/footer';
+// import Test from './components/practise';
+// import Practisetest from './components/practiseeffect';
+import CondComp from './components/condcomp';
 
 
 const App=()=>{
+  // const Alert=()=>{
+  //   alert("hello from the parent")
+  // }
+
+  // const [title, settitle]= useState("Theory");
   return(
 
   <>
-     <Navbar/>
-    <Center/>
-    <List/>
-    <Timer/>
-    <Clock/>
-    <Password/>
-    <Home/>
-    <Footer/>
-   
+  {/* <Test title={title} settitle={settitle}/> */}
+     {/* <Navbar/> */}
+    {/* <Center/> */}
+    {/* <List/> */}
+    {/* <Timer/> */}
+    {/* <Clock/> */}
+    {/* <Password/> */}
+    {/* <Home/> */}
+    {/* <Footer/> */}
+   {/* {<Practisetest/>} */}
+   <CondComp/>
    </>
   );
 }
